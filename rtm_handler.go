937d1ca9bfe9f0f7e1ea6c15e21ae493be5ba447
alice <122629939+espcaa@users.noreply.github.com>
2026-04-13 02:12:17 +0200

    feat: more websocket events

--- rtm_handler.go
+++ rtm_handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fastslack/shared"
 	"fastslack/slack"
 	"fastslack/store"
@@ -18,8 +19,32 @@ func (s *SlackService) handleRTMEvent(teamID string, event slack.RTMEvent) {
 		switch event.SubType {
 		case "message_changed":
 			log.Printf("Message edited in team %s: %s", teamID, event.Text)
+
+			// take event.raw
+			type MessageChangedEvent struct {
+				Message shared.Message `json:"message"`
+			}
+			var changedEvent MessageChangedEvent
+			if err := json.Unmarshal(event.Raw, &changedEvent); err != nil {
+				log.Printf("Failed to unmarshal message_changed event: %v", err)
+				return
+			}
+
+			store.UpsertMessage(teamID, event.Channel, changedEvent.Message)
+
+			// send event to frontend
+			app.Event.Emit("slack:message_changed", string(event.Raw))
+
 		case "message_deleted":
 			log.Printf("Message deleted in team %s: %s", teamID, event.Text)
+
+			err := store.DeleteMessage(teamID, event.Channel, event.ThreadTs, event.Ts)
+			if err != nil {
+				log.Printf("Failed to delete message from database: %v", err)
+			}
+
+			// send event to frontend
+			app.Event.Emit("slack:message_deleted", string(event.Raw))
 		default:
 			log.Printf("New message in team %s: %s", teamID, event.Text)
 			store.UpsertMessage(teamID, event.Channel, shared.Message{
